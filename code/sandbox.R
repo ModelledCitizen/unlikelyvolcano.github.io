d34f2629ff3099b2923f8a75ab9d88f8e6d2ae35
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-03-26 16:38:45 -0400

    reorg

---
+++ code/sandbox.R
@@ -0,0 +1,62 @@
+UnlikelyTools::set_wd("unlikelyvolcano.github.io")
+
+
+library(purrr)
+library(plyr)
+library(RColorBrewer)
+library(animation)
+
+jhu_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
+daily_base <- paste0(jhu_url, "csse_covid_19_daily_reports/%s.csv")
+
+td <- as.integer(Sys.Date() - as.Date("2020-03-21"))
+
+day <- c()
+daily <- list()
+for (i in 1:(td - 1)) {
+  day[i] <- format.Date(Sys.Date() - td + i, "%m-%d-%Y")
+  daily_url <- sprintf(daily_base, day[i])
+  temp <- read.csv(daily_url, stringsAsFactors = F)
+  temp <- temp[temp$Country_Region %in% "US", ]
+  temp$FIPS <- sprintf("%05d", temp$FIPS)
+  #temp <- temp[!duplicated(temp$FIPS), c("FIPS", "Confirmed", "Deaths", "Recovered", "Active")]
+  temp <- temp[!duplicated(temp$FIPS), c("FIPS", "Confirmed")]
+  names(temp) <- c("FIPS", paste(names(temp)[-1], day[i], sep = "_"))
+  daily[[day[i]]] <- temp
+}
+
+combined <- reduce(daily, join)
+
+combined[, -1] <- apply(combined[, -1], 2, function(x) {ifelse(x > 0, log(x), 0)})
+
+for (i in 3:ncol(combined)) {
+  combined[combined[ , i] < combined[ , i - 1], i] <- combined[combined[ , i] < combined[ , i - 1], i - 1]
+}
+
+brks <-
+  scales::cbreaks(c(min(unlist(combined[, -1])), max(unlist(combined[, -1]))))$breaks
+
+combined[, -1] <-
+  apply(combined[, -1], 2, function(x) {
+    cut(x, breaks = brks, labels = brewer.pal(4, "YlOrRd"))
+  })
+
+
+us_map <- readRDS("data/us_counties.RDS")
+
+
+us_cases <- merge(us_map, combined, by.x = "GEOID", by.y = "FIPS")
+x <- c(43, 72, 52, 78, 74, 69, 76, 79, 95, 70, 86, 67, 89, 68, 71, 14, 66, 07, 64, 60, 03, 84, 15, 02)
+us_cases <- us_cases[!as.numeric(us_cases$STATEFP) %in% x,]
+
+saveGIF({
+  for (d in day) {
+    plot(
+      us_cases,
+      col = us_cases[[paste0("Confirmed_", d)]],
+      border = "grey",
+      sub = d,
+      cex.sub = 4
+    )
+  }
+}, movie.name = "spread.gif", ani.width = 2000, ani.height = 1200)
---
+++ code/simplify_geo.R
@@ -0,0 +1,27 @@
+setwd("~/unlikelyvolcano.github.io")
+
+
+# Counties ----------------------------------------------------------------
+
+map <- readOGR("data/tl_2019_us_county", stringsAsFactors = F)
+map_data <- map@data
+rownames(map_data) <- sapply(map@polygons, function(x) x@ID)
+map_simple <- gSimplify(map, tol = 0.01, topologyPreserve = TRUE)
+map_new <- SpatialPolygonsDataFrame(map_simple, map_data)
+
+saveRDS(map_new, "data/us_counties.RDS")
+
+rm(map, map_data, map_simple, map_new)
+
+
+# States ------------------------------------------------------------------
+
+map <- readOGR("data/tl_2019_us_state", stringsAsFactors = F)
+map_data <- map@data
+rownames(map_data) <- sapply(map@polygons, function(x) x@ID)
+map_simple <- gSimplify(map, tol = 0.01, topologyPreserve = TRUE)
+map_new <- SpatialPolygonsDataFrame(map_simple, map_data)
+
+saveRDS(map_new, "data/us_states.RDS")
+
+rm(map, map_data, map_simple, map_new)
