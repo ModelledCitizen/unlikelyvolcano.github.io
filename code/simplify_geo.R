b74f140554a7540c79ab247492779c917bdd2c1d
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-03-29 11:57:19 -0400

    commit

--- code/simplify_geo.R
+++ code/simplify_geo.R
@@ -19,7 +19,7 @@ rm(map, map_data, map_simple, map_new)
 map <- readOGR("data/tl_2019_us_state", stringsAsFactors = F)
 map_data <- map@data
 rownames(map_data) <- sapply(map@polygons, function(x) x@ID)
-map_simple <- gSimplify(map, tol = 0.01, topologyPreserve = TRUE)
+map_simple <- gSimplify(map, tol = 0.0001, topologyPreserve = TRUE)
 map_new <- SpatialPolygonsDataFrame(map_simple, map_data)
 
 saveRDS(map_new, "data/us_states.RDS")
