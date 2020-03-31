9a02537e3ab470e1e9fa4edc95c7520474d05b3e
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-03-31 13:13:39 -0400

    update

--- code/sandbox.R
+++ code/sandbox.R
@@ -17,3 +17,16 @@ pa.counties <-
   html_table() %>%
   (function(x) {x[[1]]}) %>%
   (function(x) {colnames(x) <- x[1,]; x[-1,]})
+
+
+
+
+read_html("https://public.tableau.com/profile/cityofphiladelphia#!/vizhome/COP-COVID-19/Stacked") %>%
+  html_nodes("#title7713620505763405234_17874758422068147630 > div.tab-textRegion-boundary > div > span > div:nth-child(3) > span") %>%
+  html_text()
+
+%>%
+  (function(x) {x[[1]]}) %>%
+  (function(x) {colnames(x) <- x[1,]; x[-1,]})
+
+
